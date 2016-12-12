c5758893e6c93233edcc9eb68c3d10df3aa287b5
mjukicbraculj <mjukic-@student.math.hr>
2016-12-12 22:41:27 +0100

    Add files via upload

---
+++ Memory/Memory/Klase/MemoryClass.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Memory.Klase
+{
+    class MemoryClass
+    {
+        public int Dim { get; set; }
+        public List<int> Pozicije;
+        private static Random rng = new Random();
+        public double VisinaKucice { get; set; }
+        public double SirinaKucice { get; set; }
+
+        public string Igrac1 { get; set; }
+        public string Igrac2 { get; set; }
+
+        public int Pogodak1 { get; set; }
+        public int Pogodak2 { get; set; }
+
+        public int Brojac { get; set; }
+        public int oldValue { get; set; }
+        public int oldPosition { get; set; }
+        public int newValue { get; set; }
+        public int newPosition { get; set; }
+        public MouseButtonEventHandler klik { get; set; }
+
+        public MemoryClass(int dimenzija, string igrac1, string igrac2)
+        {
+            Dim = dimenzija;
+            Igrac1 = igrac1;
+            Igrac2 = igrac2;
+            Pogodak1 = 0;
+            Pogodak2 = 0;
+            Pozicije = Enumerable.Range(0, 50).ToList();
+            for (int i = 0; i < 50; ++i)
+                Pozicije.Add(i);
+            Pozicije = shuffle(Pozicije);
+            Brojac = 0;
+        }
+
+        public List<Line> nacrtajPlocu(double visina, double sirina)
+        {
+            List<Line> myLines = new List<Line>();
+            VisinaKucice = visina / Dim;
+            SirinaKucice = sirina / Dim;
+            for (int i = 0; i < Dim + 1; ++i)
+            {
+                Line myLine = new Line();
+                myLine.Stroke = Brushes.Red;
+                myLine.StrokeThickness = 2;
+                myLine.X1 = 0;
+                myLine.Y1 = i * VisinaKucice;
+                myLine.X2 = sirina;
+                myLine.Y2 = myLine.Y1;
+                myLines.Add(myLine);
+            }
+
+            for (int i = 0; i < Dim + 1; ++i)
+            {
+                Line myLine = new Line();
+                myLine.Stroke = Brushes.Red;
+                myLine.StrokeThickness = 2;
+                myLine.Y1 = 0;
+                myLine.X1 = i * SirinaKucice;
+                myLine.Y2 = visina;
+                myLine.X2 = myLine.X1;
+                myLines.Add(myLine);
+            }
+            return myLines;
+        }
+
+        public List<int> shuffle(List<int> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                int value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+            return list;
+        }
+
+        public bool krajIgre()
+        {
+            for (int i = 0; i < Pozicije.Count(); ++i)
+                if (Pozicije[i] != -1)
+                    return false;
+            return true;
+        }
+    }
+    
+}
---
+++ Memory/Memory/MainWindow.xaml.cs
@@ -0,0 +1,226 @@
+﻿using Memory.Klase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memory
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MemoryClass myMemory = null;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void play(object sender, EventArgs args)
+        {
+            plocaCanvas.Children.Clear();
+            pocetakSP.Visibility = Visibility.Hidden;
+            igraUTijeku.Visibility = Visibility.Visible;
+            myMemory = new MemoryClass(10, name1TB.Text, name2TB.Text);
+            name1.Text = myMemory.Igrac1;
+            name2.Text = myMemory.Igrac2;
+            igracNaPotezu.Text = myMemory.Igrac1;
+            pogodak1.Text = Convert.ToString(myMemory.Pogodak1);
+            pogodak2.Text = Convert.ToString(myMemory.Pogodak2);
+            foreach (Line child in myMemory.nacrtajPlocu(plocaCanvas.ActualHeight, plocaCanvas.ActualWidth))
+                plocaCanvas.Children.Add(child);
+            myMemory.klik = new MouseButtonEventHandler(klikNaCanvas);
+            plocaCanvas.MouseDown += myMemory.klik;
+        }
+
+        private void klikNaCanvas(object sender, MouseButtonEventArgs args)
+        {
+            Canvas myCanvas = sender as Canvas;
+            var pos = args.GetPosition(myCanvas);
+            bool success = true;
+            if (myMemory.Brojac == 2)
+                myMemory.Brojac = 0;
+
+            if (myMemory.Brojac == 0)
+                success = nacrtaj(pos);
+            else
+            {
+                success = nacrtaj(pos);
+                if (success)
+                {
+                    plocaCanvas.MouseDown -= myMemory.klik;
+                    Task.Factory.StartNew(() =>
+                    {
+                        Thread.Sleep(1000);
+                        Dispatcher.Invoke(new Action(() =>
+                        {
+                            ponistiRundu();
+                            plocaCanvas.MouseDown += myMemory.klik;
+                        }));
+
+                    });
+                }
+            }
+            if (success)
+                ++myMemory.Brojac;
+        }
+
+        private bool nacrtaj(Point pos)
+        {
+            int selectedRedak = Convert.ToInt32(Math.Floor(pos.Y / myMemory.VisinaKucice));
+            int selectedStupac = Convert.ToInt32(Math.Floor(pos.X / myMemory.SirinaKucice));
+            int index = selectedRedak * 10 + selectedStupac;
+            string imeSlicice = "Images/" + myMemory.Pozicije[index] + ".ico";
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri(imeSlicice, UriKind.Relative));
+            img.Width = myMemory.SirinaKucice;
+            img.Height = myMemory.VisinaKucice;
+
+            if (myMemory.Pozicije[index] == -1)
+                return false;
+            if (myMemory.Brojac == 0)
+            {
+                myMemory.oldValue = myMemory.Pozicije[index];
+                myMemory.oldPosition = index;
+                Canvas.SetTop(img, selectedRedak * myMemory.VisinaKucice);
+                Canvas.SetLeft(img, selectedStupac * myMemory.SirinaKucice);
+            }
+            else
+            {
+                myMemory.newValue = myMemory.Pozicije[index];
+                myMemory.newPosition = index;
+                if (myMemory.newPosition == myMemory.oldPosition)
+                    return false;
+                Canvas.SetTop(img, selectedRedak * myMemory.VisinaKucice);
+                Canvas.SetLeft(img, selectedStupac * myMemory.SirinaKucice);
+
+            }
+            plocaCanvas.Children.Add(img);
+            return true;
+        }
+
+        private void ponistiRundu()
+        {
+            List<Image> images = plocaCanvas.Children.OfType<Image>().ToList();
+            foreach (var image in images)
+                plocaCanvas.Children.Remove(image);
+            if (myMemory.oldValue == myMemory.newValue && myMemory.oldPosition != myMemory.newPosition)
+            {
+                nacrtajPravokutnik((myMemory.oldPosition / myMemory.Dim) * myMemory.VisinaKucice,
+                                        (myMemory.oldPosition % myMemory.Dim) * myMemory.SirinaKucice);
+                nacrtajPravokutnik((myMemory.newPosition / myMemory.Dim) * myMemory.VisinaKucice,
+                                        (myMemory.newPosition % myMemory.Dim) * myMemory.SirinaKucice);
+                myMemory.Pozicije[myMemory.oldPosition] = -1;
+                myMemory.Pozicije[myMemory.newPosition] = -1;
+                if (myMemory.Igrac1 == igracNaPotezu.Text)
+                {
+                    myMemory.Pogodak1++;
+                    pogodak1.Text = Convert.ToString(myMemory.Pogodak1);
+                }
+                else
+                {
+                    myMemory.Pogodak2++;
+                    pogodak2.Text = Convert.ToString(myMemory.Pogodak2);
+                }
+                //for (int i = 0; i < myMemory.Pozicije.Count; ++i)
+                //    myMemory.Pozicije[i] = -1;
+                provjeriKrajIgre();
+            }
+            else
+            {
+                if (myMemory.Igrac1 == igracNaPotezu.Text)
+                    igracNaPotezu.Text = myMemory.Igrac2;
+                else
+                    igracNaPotezu.Text = myMemory.Igrac1;
+            }
+
+        }
+
+        private void provjeriKrajIgre()
+        {
+            if (myMemory.krajIgre())
+            {
+                krajIgre.Visibility = Visibility.Visible;
+                igraUTijeku.Visibility = Visibility.Hidden;
+                pocetakSP.Visibility = Visibility.Hidden;
+            }
+        }
+
+
+        private void nacrtajPravokutnik(double x, double y)
+        {
+            System.Windows.Shapes.Rectangle rect = new System.Windows.Shapes.Rectangle();
+            rect.Stroke = new SolidColorBrush(Colors.Black);
+            rect.Fill = new SolidColorBrush(Colors.Black);
+            rect.Width = myMemory.SirinaKucice;
+            rect.Height = myMemory.VisinaKucice;
+            Canvas.SetLeft(rect, y);
+            Canvas.SetTop(rect, x);
+            plocaCanvas.Children.Add(rect);
+        }
+
+        private void crtajCanvasOpet(object sender, EventArgs args)
+        {
+            if (myMemory != null)
+            {
+                myMemory.Brojac = 0;
+                List<Image> stareStvari = plocaCanvas.Children.OfType<Image>().ToList();
+                plocaCanvas.Children.Clear();
+                double staraVisina = myMemory.VisinaKucice;
+                double staraSirina = myMemory.SirinaKucice;
+                foreach (Line child in myMemory.nacrtajPlocu(plocaCanvas.ActualHeight, plocaCanvas.ActualWidth))
+                    plocaCanvas.Children.Add(child);
+                for (int i = 0; i < myMemory.Pozicije.Count(); ++i)
+                {
+                    if (myMemory.Pozicije[i] == -1)
+                        nacrtajPravokutnik((i / myMemory.Dim) * myMemory.VisinaKucice, (i % myMemory.Dim) * myMemory.SirinaKucice);
+                }
+            }
+
+        }
+
+        private void igraJedanIgrac(object sender, EventArgs args)
+        {
+            drugiIgracSP.Visibility = Visibility.Hidden;
+        }
+
+        private void igraDvaIgraca(object sender, EventArgs args)
+        {
+            drugiIgracSP.Visibility = Visibility.Visible;
+        }
+
+        private void startNewGame(object sender, EventArgs args)
+        {
+            plocaCanvas.MouseDown -= myMemory.klik;
+            myMemory = new MemoryClass(10, myMemory.Igrac1, myMemory.Igrac2);
+            myMemory.klik = new MouseButtonEventHandler(klikNaCanvas);
+            plocaCanvas.MouseDown += myMemory.klik;
+            crtajCanvasOpet(null, null);
+            igraUTijeku.Visibility = Visibility.Visible;
+            krajIgre.Visibility = Visibility.Hidden;
+        }
+
+        private void goHome(object sender, EventArgs ergs)
+        {
+            plocaCanvas.MouseDown -= myMemory.klik;
+            krajIgre.Visibility = Visibility.Hidden;
+            igraUTijeku.Visibility = Visibility.Hidden;
+            pocetakSP.Visibility = Visibility.Visible;
+        }
+
+    }
+}
